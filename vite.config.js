0894b163958ce678d69579c8505002e8e4d8b7ef
bertsegaye <137160650+bertsegaye@users.noreply.github.com>
2025-03-22 17:43:35 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,9 +2,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
-export default defineConfig({
-  // server: {
-  //   port: 5000, // Change the port to 5000
-  // },
-  base: '/MKaa_HR_SYSTEM/',
-});
+export default {
+  base: '/MKaa_HR_SYSTEM/',  // Set this to the repository name
+};
