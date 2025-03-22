c6b09032e99512e0b5fdfdd298844b8c0bd40f11
bertsegaye <137160650+bertsegaye@users.noreply.github.com>
2025-03-22 17:52:41 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default {
-  base: '/MKaa_HR_SYSTEM/',  // Set this to the repository name
+  base: "/MKaa_HR_SYSTEM/",  // Make sure this matches your repo name
+  plugins: [react()],
 };
